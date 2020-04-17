580d1e03c19e54423cb615e16af9b0e0e1d3be40
Francesco Ambrosetti <ambrosetti.francesco@gmail.com>
2020-04-17 13:58:13 +0200

    test for check_chain() added

--- test_haddock_format.py
+++ test_haddock_format.py
@@ -17,6 +17,8 @@ class TestHf(unittest.TestCase):
     pdbfile = 'Test_data/4G6K_ch.pdb'
     chain_id = 'A'
 
+    wrong_pdb = 'Test_data/2FJF.pdb'
+
     golden_hv = [26, 27, 28, 29, 30, 31, 32, 33, 34, 55, 56, 57, 101, 102, 103, 104, 105,
                  106, 107, 108, 146, 147, 148, 149, 150, 151, 152, 170, 171, 172, 211,
                  212, 213, 214, 215, 216]
@@ -32,6 +34,14 @@ class TestHf(unittest.TestCase):
 
         resi_numb = pdb_ren.df['ATOM']['residue_number'].to_list()
         self.assertEqual(self.golden_resi, resi_numb, "Residue number is different\n")
+
+    def test_check_chain(self):
+
+        hd_format = hf.AbHaddockFormat(self.wrong_pdb, self.chain_id)
+
+        with self.assertRaises(SystemExit) as e:
+            hd_format.check_chain()
+        self.assertEqual(e.exception.code, 1)
 
 
 if __name__ == '__main__':
